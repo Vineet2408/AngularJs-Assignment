1c4499f8386d0b3d45b5abf559da782b7af0d990
Vineet2408 <vineetd84@gmail.com>
2021-04-02 14:14:27 +0530

    first assignment

---
+++ basic-assignment-start/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningComponent,
+    SuccessComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ basic-assignment-start/src/app/success/success.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector : 'success-alert',
+  templateUrl : './success.component.html',
+  styleUrls : ['./success.component.css']
+
+})
+
+export class SuccessComponent
+{
+
+}
